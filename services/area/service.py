d07ab2c0017249bedcd187b427712985a499c113
Léo <leo.verger12@gmail.com>
2025-12-12 10:27:03 +0100

    fix: fix lint after github ci

--- services/area/service.py
+++ services/area/service.py
@@ -1,6 +1,5 @@
 from typing import List, Tuple, Optional
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
 from datetime import datetime, timedelta, date, timezone
 
 from . import schemas
